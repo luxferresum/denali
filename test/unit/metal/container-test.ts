0343094b5109a958780f9144ba84b72b4d356a44
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-02-18 23:01:24 +0100

    Fix wrong used .reverse that caused the resolver order to change

--- test/unit/metal/container-test.ts
+++ test/unit/metal/container-test.ts
@@ -82,4 +82,22 @@ test('availableForType() returns all registered instances of a type', async (t) 
   t.deepEqual(container.availableForType('foo'), ['a', 'b', 'c', 'd']);
 });
 
+test('the resolver order should not be changed by lookup methods', async (t) => {
+  const container: Container = t.context.subject();
+
+  const loaderMock = (name : string) => ({
+    retrieve: () => name,
+    loadRelative: () => name,
+    factories: new Map(),
+  }) as any;
+
+  container.loadBundleScope(loaderMock('l1'));
+  container.loadBundleScope(loaderMock('l2'));
+
+  t.deepEqual(container.lookup('foo:l1', { raw: true }), 'l1');
+  container.availableForType('foo');
+  t.deepEqual(container.lookup('foo:l1', { raw: true }), 'l1');
+  t.deepEqual(container.lookup('foo:l1', { raw: true }), 'l1');
+});
+
 test.todo('fallsback to `fallbacks` specifiers if original specifier is not found');
